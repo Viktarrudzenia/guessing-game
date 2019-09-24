0d579046811779f78b8b7bee36ec23aa8b471573
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-09-24 17:19:17 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,37 @@
 class GuessingGame {
-    constructor() {}
+  // Init default values
 
-    setRange(min, max) {
+  constructor() {
+    this.numberGuess = 0;
+    this.min = 0;
+    this.max = 0;
+  }
 
-    }
+  // Set range where our value exist
 
-    guess() {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  // guessing method which return supposed number
 
-    lower() {
+  guess() {
+    this.numberGuess = Math.round(this.max - (this.max - this.min) / 2);
+    return this.numberGuess;
+  }
 
-    }
+  // set max range to numberGuess
 
-    greater() {
+  lower() {
+    this.max = this.numberGuess;
+  }
 
-    }
+  // set min range to numberGuess
+
+  greater() {
+    this.min = this.numberGuess;
+  }
 }
 
 module.exports = GuessingGame;
